77378dd4b9c04d7f92589c0f2849e92d0721a7e5
Jahan <jahanulhaque@gmail.com>
2020-05-06 01:22:46 +0100

    Packages update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TechspertFullStack
+# MEAN Stack Weather App
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.3.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'techspert-full-stack';
+  title = 'MEAN-weather-app';
 }
